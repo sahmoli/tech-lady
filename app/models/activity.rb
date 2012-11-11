b341c8f9e5a6f4f2b4624450b1e48519f40743fe
ma wei <wma@thoughtworks.com>
2012-11-11 13:30:09 +0800

    add test for model activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,6 @@
 class Activity < ActiveRecord::Base
   attr_accessible :activity_date, :address, :description, :title
+
+  validates_presence_of :title, :activity_date, :address, :description
+
 end
