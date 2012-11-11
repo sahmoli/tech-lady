8a174751a335b8c8ca9d5ec6bc36ac7ccc687578
Metaphor <zhiheng.li.metaphor@gmail.com>
2012-11-11 18:26:17 +0800

    quick fix failed build.

--- spec/requests/activities_spec.rb
+++ spec/requests/activities_spec.rb
@@ -7,6 +7,7 @@ describe "Activities" do
 
   describe "should have activities" do
     before do
+      activity.activity_date = Time.new(2008,6,21, 13,30,0, "+00:00")
       activity.save
       visit activity_path(activity)
     end
@@ -14,7 +15,7 @@ describe "Activities" do
     describe "should have activity details" do
       it { should have_content activity.title }
       it { should have_content activity.description }
-      it { should have_content activity.activity_date}
+      it { should have_content "2008-06-21, 13:30:00"}
       it { should have_content activity.address}
     end
 
