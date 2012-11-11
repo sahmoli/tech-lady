2acbeea285a87fc34da5a0ab192002113f366569
zhewuzhou <zhewuzhou@gmail.com>
2012-11-11 15:19:20 +0800

    can upload pic

--- app/models/img.rb
+++ app/models/img.rb
@@ -1,7 +1,11 @@
 class Img < ActiveRecord::Base
-  attr_accessible :description, :path
+  attr_accessible :description, :path, :load_image_file
   validates_presence_of :path, :message => "path can not be empty"
   before_create :creation_validate
+  after_save :save_image
+  PHOTO_STORE = File.join Rails.root, 'public', 'img'
+
+  public
 
   def creation_validate
     if Img.all.size >= 4
@@ -10,4 +14,22 @@ class Img < ActiveRecord::Base
     end
     return true
   end
+
+  def load_image_file=(data)
+    p data.methods
+    p data
+    self.path = data.original_filename
+    @image_data = data
+  end
+
+  def save_image
+    if @image_data
+      name = File.join PHOTO_STORE, self.path
+      File.open(name, 'wb') do |f|
+        f.write(@image_data.read)
+      end
+      @image_data = nil
+    end
+  end
+
 end
